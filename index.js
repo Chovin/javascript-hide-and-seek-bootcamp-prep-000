6f6e1f9d17ac1c55347bd8612122d1923a727ef6
Chovin Carlson <chovin@guam.net>
2018-08-26 00:54:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+
+  return document.querySelector('div#grand-node :empty')
+  // var current = document.querySelector('div#grand-node')
+  // var next = []
+  //
+  // while (current) {
+  //
+  // }
+}
+
+function increaseRankBy(n) {
+  var lists = document.querySelectorAll('.ranked-list')
+  var items = []
+  for (var i = 0; i < lists.length; i++) {
+    items.concat(lists[i].querySelectorAll('li'))
+  }
+  for (var i = 0; i < items.length; i++) {
+    items[i].innerHTML = parseInt(items[i].innerHTML) + n
+  }
+}
